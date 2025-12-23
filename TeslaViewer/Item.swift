dd1153f2b62483f80d32e02ab1a9b39a90a6be3c
Daniel Riewe <Oxyd22@users.noreply.github.com>
2025-12-23 16:35:44 +0100

    Initial Commit

---
+++ TeslaViewer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TeslaViewer
+//
+//  Created by Daniel Riewe on 23.12.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TeslaViewerTests/TeslaViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaViewerTests.swift
+//  TeslaViewerTests
+//
+//  Created by Daniel Riewe on 23.12.25.
+//
+
+import Testing
+@testable import TeslaViewer
+
+struct TeslaViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
